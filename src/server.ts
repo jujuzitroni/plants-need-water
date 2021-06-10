c0b0946cc7d5c947670ead707a3e0ea23f4126bf
jujuzitroni <jujuzitroni@gmail.com>
2021-06-10 10:23:16 +0200

    🚑️ Forward client routing in production

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -8,6 +9,11 @@ app.use(express.static('dist/app'));
 
 // Serve storybook production bundle
 app.use('/storybook', express.static('dist/storybook'));
+
+// Handle client routing, return all requests to the app
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'app/index.html'));
+});
 
 app.get('/', (_req, res) => {
   res.send('Hello World!');
