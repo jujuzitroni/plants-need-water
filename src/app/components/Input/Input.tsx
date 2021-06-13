1bdb4d7263c26e5743864dc6b15d420c59a8b892
jujuzitroni <jujuzitroni@gmail.com>
2021-06-13 16:59:37 +0200

    💄 style input field

--- src/app/components/Input/Input.tsx
+++ src/app/components/Input/Input.tsx
@@ -4,8 +4,8 @@ import styles from './Input.module.css';
 function Input(): JSX.Element {
   return (
     <label className={styles.container}>
-      Enter new habit:
-      <input className={styles.input} />
+      Enter new habit
+      <input className={styles.input} placeholder="E.g. take a break" />
     </label>
   );
 }
