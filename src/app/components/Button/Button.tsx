db6f04e9a1e136dd2bffb4838f3a47e0b4db5470
jujuzitroni <jujuzitroni@gmail.com>
2021-06-13 17:54:28 +0200

    ♻️ make Button compoment more simple

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -2,16 +2,14 @@ import React, { ReactNode } from 'react';
 import styles from './Button.module.css';
 
 type ButtonProps = {
-  text: string;
   onClick: () => void;
-  children: ReactNode;
+  children?: ReactNode;
 };
 
-function Button({ text, onClick, children }: ButtonProps): JSX.Element {
+function Button({ onClick, children }: ButtonProps): JSX.Element {
   return (
     <button className={styles.button} onClick={onClick}>
       {children}
-      <span className={styles.button__text}>{text}</span>
     </button>
   );
 }
