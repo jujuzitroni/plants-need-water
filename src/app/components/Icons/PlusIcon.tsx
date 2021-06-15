66b13bd34ea9a420cde08252d1166a4178a16973
jujuzitroni <jujuzitroni@gmail.com>
2021-06-15 13:39:36 +0200

    ✨ add PlusIcon svg

---
+++ src/app/components/Icons/PlusIcon.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function PlusLabel(): JSX.Element {
+  return (
+    <svg width="36" height="34.188" viewBox="0 0 36 34.188">
+      <g id="Group_8" data-name="Group 8" transform="translate(-55 -696.144)">
+        <line
+          y2="26.188"
+          transform="translate(73.073 700.144)"
+          fill="none"
+          stroke="#fff"
+          stroke-linecap="round"
+          stroke-width="8"
+        />
+        <line
+          x1="28"
+          transform="translate(59 713.099)"
+          fill="none"
+          stroke="#fff"
+          stroke-linecap="round"
+          stroke-width="8"
+        />
+      </g>
+    </svg>
+  );
+}
+
+export default PlusLabel;
