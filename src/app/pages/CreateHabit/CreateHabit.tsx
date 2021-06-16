d21f14abc11bedb2c1f3955023b1370c612b2a78
jujuzitroni <jujuzitroni@gmail.com>
2021-06-16 15:35:41 +0200

    ✨ add ColorSelector component to page

--- src/app/pages/CreateHabit/CreateHabit.tsx
+++ src/app/pages/CreateHabit/CreateHabit.tsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import BackButton from '../../components/BackButton/BackButton';
 import Button from '../../components/Button/Button';
+import ColorSelector from '../../components/ColorSelector/ColorSelector';
 import HabitInput from '../../components/HabitInput/HabitInput';
 import styles from './CreateHabit.module.css';
 
 function CreateHabit(): JSX.Element {
   return (
-    <div>
+    <div className={styles.createPage}>
       <header>
         <BackButton />
       </header>
       <form>
         <HabitInput />
-        <select name="color" id="">
-          <option value="red">red</option>
-          <option value="blue">blue</option>
-          <option value="yellow">yellow</option>
-        </select>
-        <Button variant="submit" onClick={alert('submitted!')} />
+        <ColorSelector children />
+        <Button variant="submit" onClick={console.log('submitted')} />
       </form>
     </div>
   );
