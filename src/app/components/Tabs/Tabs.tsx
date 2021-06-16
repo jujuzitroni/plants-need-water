112ae69d587259f2be0276412e0e65a0c95192ae
jujuzitroni <jujuzitroni@gmail.com>
2021-06-16 21:33:36 +0200

    ✨ set active class for tab navigation

--- src/app/components/Tabs/Tabs.tsx
+++ src/app/components/Tabs/Tabs.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styles from './Tabs.module.css';
+
+function getActiveClass(pathname: string) {
+  const location = useLocation();
+  if (!location.pathname.includes(pathname)) {
+    return;
+  }
+  return styles.tabs__active;
+}
 
 function Tabs(): JSX.Element {
   return (
     <div className={styles.tabs}>
-      <Link to="">today</Link>
-      <Link to="">calendar</Link>
+      <Link className={getActiveClass('/')} to="/">
+        today
+      </Link>
+      <Link className={getActiveClass('/about')} to="/about">
+        calendar
+      </Link>
     </div>
   );
 }
