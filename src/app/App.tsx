ddbbbabffa73cb3589928b4fff8e303a0df9d63c
jujuzitroni <jujuzitroni@gmail.com>
2021-06-16 21:35:00 +0200

    🔥 remove starter code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styles from './App.module.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Tabs from './components/Tabs/Tabs';
 
 function App(): JSX.Element {
   const [count, setCount] = useState<number>(0);
@@ -8,39 +9,15 @@ function App(): JSX.Element {
   return (
     <Router>
       <div className={styles.App}>
-        <header className={styles['App-header']}>
-          <p>Hello Vite + React!</p>
-          <p>
-            <button onClick={() => setCount((count) => count + 1)}>
-              count is: {count}
-            </button>
-          </p>
-          <p>
-            Edit <code>App.tsx</code> and save to test HMR updates.
-          </p>
-          <p>
-            <a
-              className={styles['App-link']}
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-            {' | '}
-            <a
-              className={styles['App-link']}
-              href="https://vitejs.dev/guide/features.html"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Vite Docs
-            </a>
-          </p>
+        <header>
+          <Tabs />
         </header>
         <Switch>
           <Route path="/about">
-            <main>About</main>
+            <main>
+              About
+              <p>Hello Vite + React!</p>
+            </main>
           </Route>
           <Route path="/">
             <main>Home</main>
