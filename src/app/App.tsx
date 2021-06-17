483ad172ee6b8d2c95c0deafedb28a9ee7e761e1
jujuzitroni <jujuzitroni@gmail.com>
2021-06-17 12:14:10 +0200

    🔥 remove unused starter code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './App.module.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Tabs from './components/Tabs/Tabs';
 
 function App(): JSX.Element {
-  const [count, setCount] = useState<number>(0);
-
   return (
     <Router>
       <div className={styles.App}>
