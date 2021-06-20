15672f85a4a7fac4d2dacc3c3079c93418641f2c
jujuzitroni <jujuzitroni@gmail.com>
2021-06-21 00:35:50 +0200

    🚚 move colors array to config file

--- src/app/components/ColorSelector/ColorSelector.tsx
+++ src/app/components/ColorSelector/ColorSelector.tsx
@@ -1,23 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './ColorSelector.module.css';
-
-const colors = [
-  '#DFEBEB',
-  '#ECB8D4',
-  '#EC5F65',
-  '#AA87A8',
-  '#FFEA6D',
-  '#FFD9B2',
-  '#1563AA',
-  '#CB970A',
-  '#F47B71',
-  '#56A59B',
-  '#6CC4C7',
-  '#CADCDC',
-  '#FA5735',
-  '#2F96FA',
-  '#45C349',
-];
+import { colors } from '../../config';
 
 type ColorSelectorProps = {
   selectedColor: string;
