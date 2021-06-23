4fb195cc2e2c7ad25e9ab11c04e65cca77323aa0
jujuzitroni <jujuzitroni@gmail.com>
2021-06-23 16:03:46 +0200

    fix component layout

--- src/app/components/HabitItem/HabitItem.tsx
+++ src/app/components/HabitItem/HabitItem.tsx
@@ -5,17 +5,15 @@ import plant_bloom from '../../assets/plant_bloom.png';
 function HabitItem(): JSX.Element {
   return (
     <article className={styles.container}>
-      <div className={styles.content}>
-        <div className={styles.checkbox}>
-          <input
-            className={styles.checkbox__input}
-            id="checkbox"
-            type="checkbox"
-          />
-          <label htmlFor="checkbox" />
-        </div>
-        <span>this is a habit</span>
+      <div className={styles.checkbox}>
+        <input
+          className={styles.checkbox__input}
+          id="checkbox"
+          type="checkbox"
+        />
+        <label htmlFor="checkbox" />
       </div>
+      <span>this is a habit with longer text even loooonger</span>
       <div className={styles.plantCard}>
         <img src={plant_bloom} alt="" />
       </div>
