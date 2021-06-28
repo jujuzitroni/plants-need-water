47d9bbe0895ecd0d32b3ad8831ef1d9a6d419913
jujuzitroni <jujuzitroni@gmail.com>
2021-06-28 14:15:41 +0200

    ✨ add stories for different versions

--- src/app/components/PlantDetail/PlantDetail.stories.tsx
+++ src/app/components/PlantDetail/PlantDetail.stories.tsx
@@ -6,6 +6,18 @@ export default {
   component: PlantDetail,
 };
 
-export const plantCard = (): JSX.Element => (
+export const newStart = (): JSX.Element => (
+  <PlantDetail rating="empty_pot" color="#CADCDC" />
+);
+
+export const someProgress = (): JSX.Element => (
+  <PlantDetail rating="fresh_plant" color="#CADCDC" />
+);
+
+export const moreProgress = (): JSX.Element => (
   <PlantDetail rating="coming_along" color="#CADCDC" />
+);
+
+export const finalStage = (): JSX.Element => (
+  <PlantDetail rating="bloom" color="#CADCDC" />
 );
