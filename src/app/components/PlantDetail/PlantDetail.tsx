1ec5dc214964a4e3211ca2c98ae0b87dbcab0400
jujuzitroni <jujuzitroni@gmail.com>
2021-06-28 12:01:06 +0200

    ✨ add getTagline function

--- src/app/components/PlantDetail/PlantDetail.tsx
+++ src/app/components/PlantDetail/PlantDetail.tsx
@@ -7,14 +7,29 @@ type PlantDetailProps = {
   color: string;
 };
 
+function getTagline(rating: string) {
+  if (rating === 'empty_pot') {
+    return "let's do this";
+  }
+  if (rating === 'fresh_plant') {
+    return 'keep at it';
+  }
+  if (rating === 'coming_along') {
+    return 'you are doing great';
+  }
+  return 'great job';
+}
+
 function PlantDetail({ rating, color }: PlantDetailProps): JSX.Element {
+  const tagline = getTagline(rating);
+
   return (
     <article
       className={styles.plantCard}
       style={{ borderBottom: `8px solid ${color}` }}
     >
       <HabitPlant rating={rating} />
-      <p>this is not the final text</p>
+      <p>{tagline}</p>
     </article>
   );
 }
