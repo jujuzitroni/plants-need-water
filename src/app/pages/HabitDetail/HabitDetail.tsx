4a5ead7276b17f8935f5bf0ef288f66809bfd6c5
jujuzitroni <jujuzitroni@gmail.com>
2021-06-30 14:56:02 +0200

    ✨ add content and styles to page

--- src/app/pages/HabitDetail/HabitDetail.tsx
+++ src/app/pages/HabitDetail/HabitDetail.tsx
@@ -3,8 +3,9 @@ import { useParams } from 'react-router-dom';
 import { Habit } from '../../App';
 import BackButton from '../../components/BackButton/BackButton';
 import Button from '../../components/Button/Button';
+import DeleteIcon from '../../components/Icons/DeleteIcon';
 import PlantDetail from '../../components/PlantDetail/PlantDetail';
-
+import { format } from 'date-fns';
 import styles from './HabitDetail.module.css';
 
 type HabitDetailProps = {
@@ -18,7 +19,8 @@ type HabitDetailParams = {
 function HabitDetail({ habits }: HabitDetailProps): JSX.Element {
   const { id } = useParams<HabitDetailParams>();
   const habit = habits.find((item) => item.id === id);
-  const { rating, color } = habit as Habit;
+  const { rating, color, name, dateCreated, datesCompleted } = habit as Habit;
+  const formattedDate = format(dateCreated, 'd.L.');
 
   return (
     <div className={styles.DetailPage}>
@@ -26,11 +28,24 @@ function HabitDetail({ habits }: HabitDetailProps): JSX.Element {
         <BackButton />
       </header>
       <main>
+        <h1>{name}</h1>
         <PlantDetail rating={rating} color={color} />
-        <p>coming soon</p>
+        <article className={styles.stat}>
+          <span>started tracking: </span>
+          <span>{formattedDate}</span>
+        </article>
+        <article className={styles.stat__completed}>
+          <span>
+            completed on <span>{datesCompleted.length}</span> days
+          </span>
+        </article>
       </main>
       <footer>
-        <Button variant="delete" />
+        <div className={styles.footer__bar} />
+        <Button variant="delete">
+          <DeleteIcon />
+          Delete Habit
+        </Button>
       </footer>
     </div>
   );
