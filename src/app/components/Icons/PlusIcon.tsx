414a542bcfdbb9d8cca1c29b65c25d94ed68876f
jujuzitroni <jujuzitroni@gmail.com>
2021-07-04 00:10:50 +0200

    🐛 fix svg component

--- src/app/components/Icons/PlusIcon.tsx
+++ src/app/components/Icons/PlusIcon.tsx
@@ -9,16 +9,16 @@ function PlusLabel(): JSX.Element {
           transform="translate(73.073 700.144)"
           fill="none"
           stroke="var(--secondary-font-color)"
-          stroke-linecap="round"
-          stroke-width="8"
+          strokeLinecap="round"
+          strokeWidth="8"
         />
         <line
           x1="28"
           transform="translate(59 713.099)"
           fill="none"
           stroke="var(--secondary-font-color)"
-          stroke-linecap="round"
-          stroke-width="8"
+          strokeLinecap="round"
+          strokeWidth="8"
         />
       </g>
     </svg>
